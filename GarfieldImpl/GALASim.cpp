052d09bdd4d010ffe7109af3983de14191380b53
SamueleTorelli <torelli.1654130@studenti.uniroma1.it>
2025-03-16 13:40:26 +0100

    stable version

--- GarfieldImpl/GALASim.cpp
+++ GarfieldImpl/GALASim.cpp
@@ -25,6 +25,29 @@
 
 using namespace Garfield;
 
+std::ofstream outFile; // Global file pointer
+
+int ev;
+
+void openFile() {
+    outFile.open("example.txt", std::ios::out | std::ios::app); // Open for writing, append mode
+    if (!outFile) {
+        std::cerr << "Error opening file!" << std::endl;
+    }
+}
+
+void writeToFile(int ev,double x, double y, double z, double t) {
+
+  outFile << ev << " " << x << " " << y << " " << z << " " << t << "\n" ;
+   
+}
+
+void closeFile() {
+    if (outFile.is_open()) {
+        outFile.close();
+    }
+}
+
 
 void userHandleStep(double x, double y, double z, double t,double e, double dx, double dy, double dz, bool hole){
   if(z>0.4)    std::cout << x << "\t" << y << "\t" << z << "\t" << e << "\t" << dx << "\t" << dy << "\t" << dz << "\t" << hole << "\n";
@@ -32,32 +55,30 @@ void userHandleStep(double x, double y, double z, double t,double e, double dx, 
 
 void userTracking(double x, double y, double z, double t,
                 int type, int level, Garfield::Medium* /*1e-2*/) {
-
     // Skip inelastic collisions that are not excitations.
     if (type != 4) return; //save tracks ONLY of	inelastic collisions that have produced	excitations.
-
-    std::cout << x << " " << y << " " << z << " " << t << std::endl;
-
+    
+    std::cout<< ev << " " << x << " " << y << " " << z << " " << t << std::endl;
+    writeToFile(ev,x,y,z,t);
 }
 
 
 
 int main() {
 
-   TApplication app("app", nullptr, nullptr);
-  
-
+  TApplication app("app", nullptr, nullptr);
+    
   MediumMagboltz gas;
   gas.LoadGasFile("/Users/samuele/Documents/Postdoc/GALA/GarfieldGALADSim/GasFile/gases/xe_5bar.gas");
   gas.SetTemperature(293.15); // 20°C
   gas.SetPressure(760.*5);      // 1 atm
   gas.Initialise(true); // Enable Penning transfer
-  
+  gas.EnableDrift(true);
   
   // Define a Teflon medium
-  Medium* teflon = new Medium();
-  teflon->SetDielectricConstant(2.1);
-  teflon->EnableDrift(false);
+  Medium teflon;
+  teflon.SetDielectricConstant(2.1);
+  teflon.EnableDrift(false);
   
   // Create a geometry
   GeometrySimple geometry;
@@ -98,17 +119,6 @@ int main() {
 	    << xmax << ", " << ymax << ", " << zmax << ")\n";
   std::cout << "Hole radius: " << hole_rad << ", Pitch: " << pitch << "\n";
   
-  // Create the gas box
-  double gasCenterX = (xminw + xmaxw) / 2;
-  double gasCenterY = (yminw + ymaxw) / 2;
-  double gasCenterZ = (zminw + zmaxw) / 2;
-  double gasSizeX = (xmaxw - xminw) / 2;
-  double gasSizeY = (ymaxw - yminw) / 2;
-  double gasSizeZ = (zmaxw - zminw) / 2;
-
-  auto gasBox = new Garfield::SolidBox(gasCenterX, gasCenterY, gasCenterZ, gasSizeX, gasSizeY, gasSizeZ);
-  geometry.AddSolid(gasBox, &gas);
-
   // Create the Teflon box
   double teflonCenterX = (xmin + xmax) / 2;
   double teflonCenterY = (ymin + ymax) / 2;
@@ -117,8 +127,8 @@ int main() {
   double teflonSizeY = (ymax - ymin) / 2;
   double teflonSizeZ = (zmax - zmin) / 2;
 
-  auto teflonBox = new Garfield::SolidBox(teflonCenterX, teflonCenterY, teflonCenterZ, teflonSizeX, teflonSizeY, teflonSizeZ);
-  geometry.AddSolid(teflonBox, teflon);
+  auto teflonBox = new Garfield::SolidBox(teflonCenterX, teflonCenterY, teflonCenterZ, teflonSizeX, teflonSizeY, teflonSizeZ+0.1);
+  geometry.AddSolid(teflonBox, &teflon);
 
   // Create 9 cylindrical holes in the Teflon box
   std::vector<std::pair<double, double>> holePositions = {
@@ -139,9 +149,20 @@ int main() {
     double z = teflonCenterZ; // Center of the hole in z
     double height = zmax - zmin; // Height of the hole
 
-    auto hole = new Garfield::SolidTube(x, y, z, hole_rad, height/2+0.01);
+    auto hole = new Garfield::SolidTube(x, y, z, hole_rad, height/2);
     geometry.AddSolid(hole, &gas); // Assign gas medium to the hole
   }
+
+  // Create the gas box
+  double gasCenterX = (xminw + xmaxw) / 2;
+  double gasCenterY = (yminw + ymaxw) / 2;
+  double gasCenterZ = (zminw + zmaxw) / 2;
+  double gasSizeX = (xmaxw - xminw) / 2;
+  double gasSizeY = (ymaxw - yminw) / 2;
+  double gasSizeZ = (zmaxw - zminw) / 2;
+  
+  auto gasBox = new Garfield::SolidBox(gasCenterX, gasCenterY, gasCenterZ, gasSizeX, gasSizeY, gasSizeZ);
+  geometry.AddSolid(gasBox, &gas);
 
   // Print success message
   std::cout << "Geometry created successfully!" << std::endl;
@@ -149,7 +170,8 @@ int main() {
   // Create the field component
   ComponentGrid* field = new ComponentGrid();
   field->LoadElectricField("/Users/samuele/Documents/Postdoc/GALA/EFSimulation/OutFileField/EFieldFile_4mesh.txt", "XYZ", false, false);
-  field->SetMedium(&gas);
+  field->SetGeometry(&geometry);
+  //field->SetMedium(&gas);
   
   // Create a sensor and associate the field component
   Sensor sensor;
@@ -160,17 +182,24 @@ int main() {
 
   new TCanvas();
   TH2D* h = new TH2D("EF","EF",int((xmaxw-xminw)/0.01),xminw,xmaxw,int((zmaxw-zminw)/0.01),zminw,zmaxw);
-
-  for(double x =  xminw; x <xmaxw; x += 0.01){
-    for (double z = zminw; z <zmaxw; z += 0.01) {
+  
+  for(double x =  xminw+0.01; x <xmaxw-0.01; x += 0.001){
+    for (double z = zminw; z <zmaxw; z += 0.001) {
       double ex, ey, ez;
 
       ex = field->ElectricField(x, 0.0, z)[0];
       ey = field->ElectricField(x, 0.0, z)[1];
       ez = field->ElectricField(x, 0.0, z)[2];
+
+      Medium* m = field->GetMedium(x, 0, z);
+      if (!m) {
+	std::cout << "Warning: Electron is in an undefined region!" << std::endl;
+      } else {
+	std::cout << "Warning: Medium driftable? " << m->IsDriftable() << std::endl;
+      }
       
-      double magn = sqrt(ex*ex+ez*ez);
-      std::cout << h->GetXaxis()->FindBin(x) << " " << h->GetYaxis()->FindBin(z) << " " << magn << std::endl;
+      double magn = sqrt(ex*ex+ey*ey+ez*ez);
+      if(z>0.2 && z<0.4 && magn < 1) std::cout <<"Magn 0 at (x,z) = " << x << " " << z << std::endl;
       h->SetBinContent(h->GetXaxis()->FindBin(x),h->GetYaxis()->FindBin(z),magn);
     }
   }
@@ -192,28 +221,35 @@ int main() {
   avalanche.EnablePlotting(&viewDrift);
   avalanche.EnableExcitationMarkers(true);
   avalanche.SetElectronTransportCut(0.00001);
-  //avalanche.SetUserHandleInelastic(userTracking);
-  
-  
-  for(int i=0;i<5;i++){
-    // Set the initial position of the electron (in cm)
-    double x0 = rand()/(double)RAND_MAX*0.3-0.15;
-    double y0 = rand()/(double)RAND_MAX*0.3-0.15;
-    double z0 = -0.25; // Start at the bottom of the volume
-    std::cout << i <<" e- starting at " << x0 << " " << y0 << " " << z0 << std::endl;
-    double t0 = 0.0; // Initial time (in ns)
-    double e0 = 0.01; // Initial energy of the electron (in eV)
-    
-    // Simulate the electron drift within the amplification volume
-    avalanche.DriftElectron(x0, y0, z0, t0, e0);
+  avalanche.SetUserHandleInelastic(userTracking);
 
-    AnalyseAval(avalanche,field);
+  bool debug = true;
+  
+  if(!debug){
+    openFile();
     
+    std::cout << "Drifting medium: "<< gas.IsDriftable() << " " << teflon.IsDriftable() << "\n"; 
+    
+    for(int i=0;i<1;i++){
+      // Set the initial position of the electron (in cm)
+      double x0 = rand()/(double)RAND_MAX*0.3-0.15;
+      double y0 = rand()/(double)RAND_MAX*0.3-0.15;
+      double z0 = -0.25; // Start at the bottom of the volume
+      std::cout << i <<" e- starting at " << x0 << " " << y0 << " " << z0 << std::endl;
+      double t0 = 0.0; // Initial time (in ns)
+      double e0 = 0.01; // Initial energy of the electron (in eV)
+      
+      // Simulate the electron drift within the amplification volume
+      avalanche.DriftElectron(x0, y0, z0, t0, e0);
+      ev=i;
+      AnalyseAval(avalanche,field);
+      
+    }
+  
+    closeFile();
   }
   viewDrift.Plot();
 
-  
-  
   // Visualize the electron's path
     
   // Visualize the geometry
